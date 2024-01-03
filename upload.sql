18f333a98cb72fa5ed9c63e439094480114211a1
Ivan Policarpio <144110051+Poli010@users.noreply.github.com>
2024-01-03 16:28:35 +0800

    Add files via upload

--- upload.sql
+++ upload.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 03, 2024 at 05:37 AM
+-- Generation Time: Jan 03, 2024 at 09:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,6 +32,7 @@ CREATE TABLE `add_to_cart` (
   `image` varchar(255) NOT NULL,
   `product_price` int(255) NOT NULL,
   `product_description` text NOT NULL,
+  `product_size` varchar(255) NOT NULL,
   `quantity` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,9 +40,12 @@ CREATE TABLE `add_to_cart` (
 -- Dumping data for table `add_to_cart`
 --
 
-INSERT INTO `add_to_cart` (`name`, `image`, `product_price`, `product_description`, `quantity`) VALUES
-('img/6594c64648291.jpg', 'G-Shock', 500, '  capalogs', 0),
-('img/6594c64648291.jpg', 'G-Shock', 500, '  capalogs', 0);
+INSERT INTO `add_to_cart` (`name`, `image`, `product_price`, `product_description`, `product_size`, `quantity`) VALUES
+('img/6595053e58208.jpg', 'Bench', 650, '    Shorts', 'L', 1),
+('img/6595053e58208.jpg', 'Bench', 650, '    Shorts', 'M', 4),
+('img/6595053e58208.jpg', 'Bench', 650, '    Shorts', 'L', 2),
+('img/6595053e58208.jpg', 'Bench', 650, '    Shorts', 'S', 30),
+('img/6595053e58208.jpg', 'Bench', 650, '    Shorts', 'S', 5);
 
 -- --------------------------------------------------------
 
@@ -52,6 +56,7 @@ INSERT INTO `add_to_cart` (`name`, `image`, `product_price`, `product_descriptio
 CREATE TABLE `review` (
   `id` int(11) NOT NULL,
   `comments` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   `rating` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -60,12 +65,10 @@ CREATE TABLE `review` (
 -- Dumping data for table `review`
 --
 
-INSERT INTO `review` (`id`, `comments`, `image`, `rating`) VALUES
-(1, 'asd', '6594cf598747f.jpg', 5),
-(2, 'asd', '6594cfb727a73.jpg', 5),
-(3, 'asdadsas', '6594d09cd653c.jpg', 2),
-(4, 'jakol', '6594d2391683b.jpg', 3),
-(5, '', '6594d988d062d.jpg', 1);
+INSERT INTO `review` (`id`, `comments`, `product_id`, `image`, `rating`) VALUES
+(1, 'Ganda ng short magugustuhan to ng asawa ko', 'product_id-6595053e58200', '6595080685e1c.jpg', 5),
+(2, 'ANG PANGET NG SHORT DI SUKAT YUNG SIZE MAG SARA NA KAYO', 'product_id-6595053e58200', '6595088888b5d.jpg', 1),
+(3, 'hehe', 'product_id-6595053e58200', '659508ea4b739.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -75,6 +78,7 @@ INSERT INTO `review` (`id`, `comments`, `image`, `rating`) VALUES
 
 CREATE TABLE `tb_upload` (
   `id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
   `name` varchar(50) NOT NULL,
   `product_price` varchar(255) NOT NULL,
   `product_description` varchar(255) NOT NULL,
@@ -87,8 +91,8 @@ CREATE TABLE `tb_upload` (
 -- Dumping data for table `tb_upload`
 --
 
-INSERT INTO `tb_upload` (`id`, `name`, `product_price`, `product_description`, `product_type`, `product_amount`, `image`) VALUES
-(17, 'G-Shock', '500', 'capalogs', 'Warch', '12', '6594c64648291.jpg');
+INSERT INTO `tb_upload` (`id`, `product_id`, `name`, `product_price`, `product_description`, `product_type`, `product_amount`, `image`) VALUES
+(3, 'product_id-6595053e58200', 'Bench', ' 650', '  Shorts', ' Short pang malakasan', ' 25', '6595053e58208.jpg');
 
 --
 -- Indexes for dumped tables
@@ -114,13 +118,13 @@ ALTER TABLE `tb_upload`
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tb_upload`
 --
 ALTER TABLE `tb_upload`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
