6d30ff2cdf8264f08bdea9c2bb0bc62b42825c87
Clipsqt <144028883+Clipsqt@users.noreply.github.com>
2024-01-04 14:25:31 +0800

    Add files via upload

---
+++ rider_acc.php
@@ -0,0 +1,59 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courier Account</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="rider_acc.css">
+    <script src="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <header>
+        <img  id="logo" src="logo1.png" alt="">
+        <hr>
+    </header>
+    
+    
+    <div class="container" id="container">
+                <div class="col-md-3" id="profile">
+                    <div class="card" id="pic">
+                        <div class="card-body">
+                 
+                        </div>
+                    </div>
+                </div>
+                 <p> Angelo Capa</p>
+                 <p1>rurilhayne@gmail.com</p1>
+                 <p2>09672938293</p2>
+                 <p3>San jose Del Monte Bulacan</p3>
+        </div>
+    <div class="col-md-3" id="info">
+                <div class="card" id="data">
+                    <div class="card-body">
+                    <a href="#" class="orders-link"><i class="fas fa-shopping-cart"></i> Orders</a>
+                    <a href="#" class="refunds-link "><i class="fas fa-sync"></i> Refunds</a> <br><br>
+                    <a href="#" class="success-link "><i class="fas fa-check-circle"></i> Successful Delivery</a> <br> <br>
+                    <a href="#" class="account-link"> <i class="fas fa-cogs"></i> Account Settings</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+
