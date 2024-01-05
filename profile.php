e1fa1129178a6c67c17f44a0425212815a4ad564
Clipsqt <144028883+Clipsqt@users.noreply.github.com>
2024-01-05 15:24:15 +0800

    Add files via upload

--- profile.php
+++ profile.php
@@ -7,44 +7,24 @@
     <link rel="stylesheet" href="profile.css">
 </head>
 <body>
-    <div class="container-fluid">
-        <header>
-            <img src="logo1.png" alt="">
-            <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                        <a class="nav-link"><i class="fas fa-bell"></i></a>
-                            <a class="nav-link" href="goldtags_apparel.php">Home</a>
-                            <a class="nav-link" href="#">Pricing</a>
-                            <a class="nav-link" href="#">About</a>
-                            <p id="userEmail"></p>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-            <hr>
-        </header>
-  
-
-        <h4 style="text-align: center; font-size: 24px;">User<h4>
-        
-            <div class="user_profile">
-                <div class="credentials">
-                <img id="profilePicture" src="none.png" alt="Profile Picture" style="width: 100px; height: 100px; border-radius: 50%; cursor: pointer;">
-                <input type="file" id="profilePictureInput" style="display: none;">
+        <div class="container-fluid">
+        <img src="logo1.png" alt="" class="pic">
+                <p id="userEmail"></p>
+                <hr>
+                <h1>PROFILE</h1>
+    </div>
+        <div class="userprofile">
+        <div class="credentials">
+                <img id="profilePicture" src="none.png" alt="Profile Picture">
+                <input type="file" id="profilePictureInput">
                 <label for="profilePictureInput" id="uploadLabel" style="display: none;">Upload</label>
                     <p id="firstName"></p><p id="lastName"></p>
                         <div class="email">
                         <p id="email"></p>
-                        </div>
-                </div>
-
-                <div class="address">
-                    <h3>Phone Number: </h3><p id="phoneNumber"></p></br>
+        </div>            
+    </div>
+         <div class="address">
+                    <h3>Phone Number:</h3><p id="phoneNumber"></p></br>
                     <h3>Street Name: </h3><p id="streetName"></p></br>
                     <h3>Street Number: </h3><p id="streetNum"></p><br>
                     <h3>Block: </h3><p id="block"></p></br>
@@ -52,14 +32,17 @@
                     <h3>Baranggay: </h3><p id="baranggay"></p></br>
                     <h3>City: </h3><p id="city"></p></br>
                     <i class="fa-light fa-cart-shopping"></i>
-                    <p><a href="cart.php"> Cart </a></p><br>
+                <div class="button">
+                    <p><a href="cart.php" class="cart">🛒Cart </a></p><br>
                     <i class="fa-solid fa-bag-shopping"></i>
-                    <p><a href="purchase_record.php">Purchased</a></p><br>
-                    <button type="submit" id=change>Edit</button>
-                </div>
-            </div>
-</div>
+                    <p><a href="purchase_record.php" class="purchase">✓ Purchased</a></p><br>
+                 </div>
+                    <button type="submit" id=change>⚙️Edit</button>
+             </div>
+   
 
+
+        </div>
 <script type="module">
        
         import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
