0c0de2c42a8a9ae5c8cd9be27f0c12ddf56b40e2
Ivan Policarpio <144110051+Poli010@users.noreply.github.com>
2024-01-05 10:54:44 +0800

    Add files via upload

--- add_to_cart.php
+++ add_to_cart.php
@@ -4,12 +4,13 @@ require_once("connection.php");
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $image =$_POST['image'];
+    $product_id = $_POST['product_id'];
     $product_size = $_POST['size'];
     $price = $_POST['product_price'];
     $description = $_POST['product_description'];
     $quantity = $_POST['quantity'];
 
-  $sql = "INSERT INTO add_to_cart VALUES('','$name','$image', ' $price', '  $description', '$product_size', '$quantity')";
+  $sql = "INSERT INTO add_to_cart VALUES('','$name','$image','$product_id', ' $price', '  $description', '$product_size', '$quantity')";
     mysqli_query($conn, $sql);
     echo
       "
