eccdefda8e9e6e11c8394f67bce1a358c4919b82
Clipsqt <144028883+Clipsqt@users.noreply.github.com>
2024-01-06 13:05:21 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+    include ('login_server.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,75 +12,19 @@
 </head>
 <body>
     <section class="form">
-        <form id="loginForm">
+    <form id="loginForm" method="post">
             <h1>LOGIN</h1>
-           
-                <div class="input-box">
-                    <input type="email" id="email" placeholder="Email">
-                    <input type="password" id="password" placeholder="Password">
-                </div>
-                <button type="submit">Login</button>
-
-                <p1><a href="admin.php">Login as Administrator</a></p1>
-                <p2>No account? <a href="signup.html">Signup</a></p2>
-            
-        </form>
+            <div class="input-box">
+                <input type="email" id="email" name="email" placeholder="Email"> 
+                <div id="email_error" class="error"></div>
+                <input type="password" id="password" name="accountPass" placeholder="Password"> 
+                <div id="pass_error" class="error"></div> 
+            </div>
+            <button type="submit">Login</button>
+            <p1><a href="admin.php">Login as Administrator</a></p1>
+            <p2>No account? <a href="signup.php">Signup</a></p2>
+    </form>
     </section>
-    <script type="module">
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-       
-        import { getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
- 
-        const firebaseConfig = {
-            apiKey: "AIzaSyCKFLDnY6nvXO9YHXZ56k6jE4q4esbyKNw",
-            authDomain: "goldtags-afbf1.firebaseapp.com",
-            databaseURL: "https://goldtags-afbf1-default-rtdb.asia-southeast1.firebasedatabase.app",
-            projectId: "goldtags-afbf1",
-            storageBucket: "goldtags-afbf1.appspot.com",
-            messagingSenderId: "476543580017",
-            appId: "1:476543580017:web:b7a8c4afe121d3155b28d0"
-            };
-
-            // Initialize Firebase
-            const app = initializeApp(firebaseConfig);
-            const auth = getAuth(app);
-
-            document.addEventListener('DOMContentLoaded', function() {
-            const loginForm = document.getElementById('loginForm');
-
-            loginForm.addEventListener('submit', (e) => {
-                e.preventDefault();
-
-                const email = document.getElementById('email').value;
-                const password = document.getElementById('password').value;
-
-                signInWithEmailAndPassword(auth, email, password)
-                    .then((userCredential) => {
-                        const user = userCredential.user;
-                        if (user && user.emailVerified) {
-                            // Retrieve user ID and email
-                            const userId = user.uid;
-                            const userEmail = user.email;
-                            console.log("User ID:", userId);
-                            console.log("User Email:", userEmail);
-
-                            localStorage.setItem('userId', userId);
-                            localStorage.setItem('email', email);
-                            console.log("user and email stored to database");
-                            alert('Login successful!');
-                            window.location.href = 'goldtags_apparel.php';
-                        } else if (user) {
-                            alert('Email is not verified. Please verify your email.');
-                        } else {
-                            alert('User not found. Please check your email and password.');
-                        }
-                    })
-                    .catch((error) => {
-                        console.error(error.message);
-                        alert('Login failed. Please check your email and password.');
-                    });
-            });
-        });
-      </script>
+    <script src="login.js"></script> 
 </body> 
 </html>
