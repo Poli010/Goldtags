a85214c05e4ecd8aed79623422cfd7aabaeaf576
Clipsqt <144028883+Clipsqt@users.noreply.github.com>
2024-01-09 15:13:24 +0800

    Add files via upload

--- rider_acc.php
+++ rider_acc.php
@@ -1,59 +1,83 @@
+<?php
+session_start();
 
+if (!isset($_SESSION['email'])) {
+    header("Location: login.php");
+    exit();
+}
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "upload";
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$loggedInEmail = $_SESSION['email'];
+
+$query = "SELECT * FROM accounts WHERE email = '$loggedInEmail'";
+$result = mysqli_query($connection, $query);
+
+if ($result && mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+
+    $firstName = $row['firstName'];
+    $lastName = $row['lastName'];
+    $userName = $row['userName'];
+    $email = $row['email'];
+    $contactNumber = $row['contact_number'];
+    $profileImage = 'profile_image/' . $row['profile_image']; 
+}
+
+mysqli_close($connection);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rider Account</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="rider_acc.css">
-    <script src="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/dist/boxicons.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <title>RIDER ACCOUNT </title>
 </head>
 <body>
-    
-    <header>
-        <img  id="logo" src="logo1.png" alt="">
+    <div class="container">
+        <h1>RIDER ACCOUNT</h1>
         <hr>
-    </header>
+    <div class="info">
+            <img id="profilePicture" src="<?php echo $profileImage; ?>" alt="Profile Picture">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+          <div class="file-input-container">
+                <input type="file" name="profilePictureInput" id="profilePictureInput">
+          </div>    
+                <br>
+                <label for="profilePictureInput" id="uploadLabel">Choose a file</label>
+                <button type="submit">Upload</button>
+        </form>
     
-    
-    <div class="container" id="container">
-                <div class="col-md-3" id="profile">
-                    <div class="card" id="pic">
-                        <div class="card-body">
-                 
-                        </div>
-                    </div>
-                </div>
-                 <p4> Angelo Capa</p4>
-                 <p1>rurilhayne@gmail.com</p1>
-                 <p2>09672938293</p2>
-                 <p3>San jose Del Monte Bulacan</p3>
-        </div>
-    <div class="col-md-3" id="info">
-                <div class="card" id="data">
-                    <div class="card-body">
-                    <a href="#" class="orders-link"><i class="fas fa-shopping-cart"></i> Orders</a>
-                    <a href="#" class="refunds-link "><i class="fas fa-sync"></i> Refunds</a> <br><br>
-                    <a href="#" class="success-link "><i class="fas fa-check-circle"></i> Successful Delivery</a> <br> <br>
-                    <a href="#" class="account-link"> <i class="fas fa-cogs"></i> Account Settings</a>
-                    </div>
-                </div>
+          
+    </div>
+        <div class="data">
+            <h3>First Name:</h3><p id="firstName"><?php echo $firstName; ?></p></br>
+            <h3>Last Name: </h3><p id="lastName"><?php echo $lastName; ?></p></br>
+            <h3>User Name: </h3><p id="userName"><?php echo $userName; ?></p></br>
+            <h3>Email: </h3><p id="email"><?php echo $email; ?></p></br>
+            <h3>Phone Number: </h3><p id="contact_no"><?php echo $contactNumber; ?></p><br>
+            <i class="fa-light fa-cart-shopping"></i>
+            <div class="button">
+            <a href="profile_edit.php" id="account">Account Settings</a>
+                <p><a href="orders_for_delivery.php" class="order">Orders</a></p><br>
+                <p><a href="#" class="refund">Refunds</a></p><br>
+                <i class="fa-solid fa-bag-shopping"></i>
+                <p><a href="purchase_record.php" class="success">Successful Delivery</a></p><br>
+              
             </div>
         </div>
     </div>
-
-
-
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
-
