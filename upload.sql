89563cfcf14d42de65ef8ac65d8a84e4f713627f
Ivan Policarpio <144110051+Poli010@users.noreply.github.com>
2024-01-11 17:01:14 +0800

    Add files via upload

--- upload.sql
+++ upload.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 09, 2024 at 08:42 AM
+-- Generation Time: Jan 11, 2024 at 10:00 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,6 +34,8 @@ CREATE TABLE `accounts` (
   `email` varchar(150) NOT NULL,
   `accountPass` varchar(150) NOT NULL,
   `account_type` varchar(150) NOT NULL,
+  `verification_code` int(40) NOT NULL,
+  `signup_verification` int(40) NOT NULL,
   `contact_number` bigint(255) NOT NULL,
   `profile_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
@@ -42,14 +44,15 @@ CREATE TABLE `accounts` (
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`firstName`, `lastName`, `userName`, `email`, `accountPass`, `account_type`, `contact_number`, `profile_image`) VALUES
-('Angelo ', 'Capa', 'Clips', 'Clips@gmail.com', '123', '', 0, ''),
-('Angelo ', 'Capa', 'Clips', 'Clips@gmail.com', '123', '', 0, ''),
-('123', '123', 'cute', 'cute@gmail.com', '123', '', 0, ''),
-('ivan', 'policarpio', 'poli', 'ivanpolicarpio@gmail.com', '123', '', 0, ''),
-('Rhaven', 'Quintana', 'killuah', 'rhaven@gmail.com', '123', 'rider', 0, ''),
-('Jonathan', 'Aguirre', 'Jakol', 'Jakol@gmail.com', '123', 'rider', 0, ''),
-('Ivan', 'Policarpio', 'Poli', 'ivanpolicarpio015@gmail.com', '123', 'rider', 0, '');
+INSERT INTO `accounts` (`firstName`, `lastName`, `userName`, `email`, `accountPass`, `account_type`, `verification_code`, `signup_verification`, `contact_number`, `profile_image`) VALUES
+('Angelo ', 'Capa', 'Clips', 'Clips@gmail.com', '123', '', 0, 0, 0, ''),
+('Angelo ', 'Capa', 'Clips', 'Clips@gmail.com', '123', '', 0, 0, 0, ''),
+('123', '123', 'cute', 'cute@gmail.com', '123', '', 0, 0, 0, ''),
+('ivan', 'policarpio', 'poli', 'ivanpolicarpio@gmail.com', '123', '', 0, 0, 9485905921, 'Capture2.PNG'),
+('Rhaven', 'Quintana', 'killuah', 'rhaven@gmail.com', '123', 'rider', 0, 0, 0, ''),
+('Jonathan', 'Aguirre', 'Jakol', 'Jakol@gmail.com', '123', 'rider', 0, 0, 0, ''),
+('Ivan', 'Policarpio', 'Poli', 'ivanpolicarpio015@gmail.com', '123', 'rider', 0, 0, 0, ''),
+('Admin', 'Admin', 'Admin', 'admin@gmail.com', '123', 'Admin', 0, 0, 9485905921, '');
 
 -- --------------------------------------------------------
 
@@ -60,21 +63,58 @@ INSERT INTO `accounts` (`firstName`, `lastName`, `userName`, `email`, `accountPa
 CREATE TABLE `add_to_cart` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `buyer_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   `product_id` varchar(255) NOT NULL,
   `product_price` int(255) NOT NULL,
   `product_description` text NOT NULL,
   `product_size` varchar(255) NOT NULL,
-  `quantity` int(255) NOT NULL
+  `quantity` int(255) NOT NULL,
+  `contact_number` bigint(20) NOT NULL,
+  `cancel_button_hide` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_pending`
+--
+
+CREATE TABLE `customer_pending` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `buyer_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `product_price` int(255) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `product_size` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `baranggay` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `province` varchar(255) NOT NULL,
+  `zip_code` int(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `contact_number` bigint(20) NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `cancel_button_hide` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `add_to_cart`
+-- Dumping data for table `customer_pending`
 --
 
-INSERT INTO `add_to_cart` (`id`, `username`, `name`, `image`, `product_id`, `product_price`, `product_description`, `product_size`, `quantity`) VALUES
-(5, 'cute', 'asdasdasd', '65991ffe8a934.png', 'product_id-65991ffe8a92c', 12, '    12', 'L', 6);
+INSERT INTO `customer_pending` (`id`, `name`, `product_id`, `buyer_name`, `email`, `product_price`, `quantity`, `product_size`, `address`, `baranggay`, `city`, `province`, `zip_code`, `image`, `contact_number`, `time_stamp`, `cancel_button_hide`) VALUES
+(36, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 2, 'S', 'asd', 'asd', 'asd', 'asd', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:15:20', 0),
+(37, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 1, 'S', 'asd', 'asd', 'asd', 'asd', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:15:51', 0),
+(38, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 1, 'S', 'asd', 'asd', 'asd', 'as', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:23:22', 0),
+(39, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 2, 'M', 'asd', 'asd', 'asd', 'asd', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:23:31', 0),
+(40, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 1, 'S', 'asd', 'asd', 'asd', 'asd', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:31:08', 0),
+(41, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 1, 'M', 'asd', 'asd', 'asd', 'asd', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:31:20', 0),
+(43, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 2, 'S', 'asd', 'asd', 'asd', 'asd', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:51:07', 0),
+(44, 'Nike', 'product_id-659f44c36f193', 'ivan policarpio', 'ivanpolicarpio@gmail.com', 500, 1, 'S', 'asd', 'asd', 'asd', 'asd', 0, '659f44c36f19f.jpg', 9485905921, '2024-01-11 08:57:21', 0);
 
 -- --------------------------------------------------------
 
@@ -85,9 +125,11 @@ INSERT INTO `add_to_cart` (`id`, `username`, `name`, `image`, `product_id`, `pro
 CREATE TABLE `for_delivery` (
   `id` int(11) NOT NULL,
   `buyer_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `contact_number` bigint(20) NOT NULL,
   `address` varchar(255) NOT NULL,
   `product_name` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
   `product_price` int(255) NOT NULL,
   `quantity` int(255) NOT NULL,
   `size` varchar(255) NOT NULL,
@@ -99,9 +141,13 @@ CREATE TABLE `for_delivery` (
 -- Dumping data for table `for_delivery`
 --
 
-INSERT INTO `for_delivery` (`id`, `buyer_name`, `contact_number`, `address`, `product_name`, `product_price`, `quantity`, `size`, `rider`, `image`) VALUES
-(5, 'asd', 9232182312, 'asd  asd asd asd 12093', 'Nike', 24, 5, 'M', 'Ivan Policarpio', ''),
-(6, 'asd', 12312312312, 'asd asd asd asd 3123', 'Austine', 300, 50, 'M', 'Ivan Policarpio', '659ce7125dc72.jpg');
+INSERT INTO `for_delivery` (`id`, `buyer_name`, `email`, `contact_number`, `address`, `product_name`, `product_id`, `product_price`, `quantity`, `size`, `rider`, `image`) VALUES
+(18, 'ivan policarpio', '', 9485905921, 'asd asd asd asd 0', 'Nike', '', 500, 1, 'S', 'Ivan Policarpio', '659f44c36f19f.jpg'),
+(19, 'ivan policarpio', '', 9485905921, 'asd asd asd asd 0', 'Nike', '', 500, 2, 'M', '', '659f44c36f19f.jpg'),
+(20, 'ivan policarpio', '', 9485905921, 'asd asd asd asd 0', 'Nike', '', 500, 2, 'M', 'Ivan Policarpio', '659f44c36f19f.jpg'),
+(21, 'ivan policarpio', '', 9485905921, 'asd asd asd asd 0', 'Nike', '', 500, 1, 'M', 'Ivan Policarpio', '659f44c36f19f.jpg'),
+(22, 'ivan policarpio', 'ivanpolicarpio@gmail.com', 9485905921, 'asd asd asd asd 0', 'Nike', '', 500, 2, 'S', 'Ivan Policarpio', '659f44c36f19f.jpg'),
+(23, 'ivan policarpio', 'ivanpolicarpio@gmail.com', 9485905921, 'asd asd asd asd 0', 'Nike', 'product_id-659f44c36f193', 500, 1, 'S', 'Jonathan Aguirre', '659f44c36f19f.jpg');
 
 -- --------------------------------------------------------
 
@@ -112,6 +158,9 @@ INSERT INTO `for_delivery` (`id`, `buyer_name`, `contact_number`, `address`, `pr
 CREATE TABLE `pending` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `buyer_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `product_price` int(255) NOT NULL,
   `quantity` int(255) NOT NULL,
   `product_size` varchar(255) NOT NULL,
@@ -121,21 +170,9 @@ CREATE TABLE `pending` (
   `province` varchar(255) NOT NULL,
   `zip_code` int(255) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `contact_number` bigint(20) DEFAULT NULL
+  `contact_number` bigint(20) DEFAULT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `pending`
---
-
-INSERT INTO `pending` (`id`, `name`, `product_price`, `quantity`, `product_size`, `address`, `baranggay`, `city`, `province`, `zip_code`, `image`, `contact_number`) VALUES
-(6, 'Nike', 5000, 2, 'M', 'asd', 'asd', 'asd', 'asd', 123, '659c9b68b2d78.jpg', 12312312312),
-(7, 'Nike', 5000, 2, 'M', 'asd', 'asd', 'asd', 'asd', 123, '659c9b68b2d78.jpg', 12312312312),
-(8, 'Nike', 5000, 2, 'M', 'asd', 'asd', 'asd', 'asd', 123, '659c9b68b2d78.jpg', 12312312312),
-(9, 'Nike', 5000, 2, 'M', 'asd', 'asd', 'asd', 'asd', 123, '659c9b68b2d78.jpg', 12312312312),
-(10, 'Nike', 5000, 2, 'M', 'asd', 'asd', 'asd', 'asd', 1234, '659c9b68b2d78.jpg', 11231231231),
-(11, 'Nike', 5000, 5, 'S', 'asdasd', 'asdasd', 'asdasd', 'asdasd', 11234, '659c9b68b2d78.jpg', 12312313123),
-(12, 'Nike', 5000, 5, 'M', 'asd', 'asd', 'asd', 'asd', 3023, '659c9b68b2d78.jpg', 9120390192);
 
 -- --------------------------------------------------------
 
@@ -183,9 +220,9 @@ CREATE TABLE `tb_upload` (
 --
 
 INSERT INTO `tb_upload` (`id`, `product_id`, `name`, `product_price`, `product_description`, `product_type`, `product_amount`, `image`) VALUES
-(26, 'product_id-659c9b68b2d6e', 'Nike', ' 5000', '  akjhsd', ' Short', '0', '659c9b68b2d78.jpg'),
-(27, 'product_id-659ce7125dc68', 'Austine', '300', 'AJsd ajsdak jaksdjakjs kajdsha', 'Short', '100', '659ce7125dc72.jpg'),
-(28, 'product_id-659cf7de8938b', 'asd', ' 12', '  asduh akjdhsasjdh', ' Tshirt', ' 123', '659cf7de89396.jpg');
+(31, 'product_id-659f44c36f193', 'Nike', ' 500', '  In the labyrinth of life, serendipity unveils mellifluous moments of resilience.', ' Short', '53', '659f44c36f19f.jpg'),
+(32, 'product_id-659f44eb1c11f', 'Jordan', ' 500', '  In the labyrinth of life, serendipity unveils mellifluous moments of resilience.', ' tshirt', '0', '659f44eb1c129.jpg'),
+(33, 'product_id-659f9757e51ad', 'asd', ' 1231', '  asd', ' asd', '130', '659f9757e51c7.png');
 
 --
 -- Indexes for dumped tables
@@ -195,6 +232,12 @@ INSERT INTO `tb_upload` (`id`, `product_id`, `name`, `product_price`, `product_d
 -- Indexes for table `add_to_cart`
 --
 ALTER TABLE `add_to_cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer_pending`
+--
+ALTER TABLE `customer_pending`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -229,19 +272,25 @@ ALTER TABLE `tb_upload`
 -- AUTO_INCREMENT for table `add_to_cart`
 --
 ALTER TABLE `add_to_cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `customer_pending`
+--
+ALTER TABLE `customer_pending`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `for_delivery`
 --
 ALTER TABLE `for_delivery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `pending`
 --
 ALTER TABLE `pending`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `review`
@@ -253,7 +302,7 @@ ALTER TABLE `review`
 -- AUTO_INCREMENT for table `tb_upload`
 --
 ALTER TABLE `tb_upload`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
